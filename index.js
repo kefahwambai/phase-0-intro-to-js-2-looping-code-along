787e71a9ab384ae3d22a03c7933ba1d863919d56
KefahWambai <kefah.ngwei@student.moringaschool.com>
2022-11-29 14:48:55 +0300

    done

--- index.js
+++ index.js
@@ -1,36 +1,13 @@
-// Code your solutions in this file
-const gifts = ["PS5", "Car", "iphone"];
-
-function wrapGifts(gifts) {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
-    debugger;
-  }
-
-  return gifts;
-}
-
-wrapGifts(gifts);
-
-function writeCards(){
-    const names = ["Ian", "Jordan", "Keith"]
-    eventName = "birthday"
-    
-    for(i=0; i<names.length; i++){
-        newArray =[`Thank you, ${names[i]} for the wonderful ${eventName} gifts!`] 
-
-        console.log(newArray);
-        
+let name=["Oliver", "Lewis", "Kamau"]
+function writeCards(name , message) {
+    let messages=[]
+    for(let j=0;j<name.length;j++){
+        messages.push(`Thank you, ${name[j]}, for the wonderful surprise gift!`)
     }
-    return newArray;
+    return messages
 }
-writeCards();
-
-function countDown(){
-    let count =10;
-    while (count>=0){
-        console.log(count);
-        count--;
+function countDown(j){
+    for(let n=j;n>=0;n--){
+    console.log(n)    
     }
 }
-countDown();
