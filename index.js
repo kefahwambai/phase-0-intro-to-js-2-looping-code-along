4bec4350e277433e6505104a31e48703f9ed2fc0
KefahWambai <kefah.ngwei@student.moringaschool.com>
2022-11-29 14:38:27 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solutions in this file
+const gifts = ["PS5", "Car", "iphone"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(){
+    const names = ["Ian", "Jordan", "Keith"]
+    eventName = "birthday"
+    
+    for(i=0; i<names.length; i++){
+        newArray =[`Thank you, ${names[i]} for the wonderful ${eventName} gifts!`] 
+
+        console.log(newArray);
+        
+    }
+    return newArray;
+}
+writeCards();
+
+function countDown(){
+    let count =10;
+    while (count>=0){
+        console.log(count);
+        count--;
+    }
+}
+countDown();
